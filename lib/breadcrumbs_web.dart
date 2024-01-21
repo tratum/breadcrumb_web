659b68958516bb1ac4b4022e626391f0fc677072
Saksham Rawat <saksham.01rawat@gmail.com>
2024-01-21 14:27:21 +0530

    Added crumbSeperator param

--- lib/breadcrumbs_web.dart
+++ lib/breadcrumbs_web.dart
@@ -18,9 +18,10 @@ class BreadcrumbTrail {
     String crumbFontFamily = 'Roboto',
     FontWeight crumbFontWeight = FontWeight.w600,
     Color crumbFontColor = const Color(0xff121212),
+    double crumbFontSize = 16,
     double crumbSpacing = 8.0,
     double scaleOnHover = 12.0,
-    double crumbFontSize = 16,
+    Widget? crumbSeparator,
     IconData crumbIcon = Icons.arrow_forward_ios_sharp,
     double crumbIconSize = 14,
     Color crumbIconColor = const Color(0xff121212),
@@ -33,6 +34,7 @@ class BreadcrumbTrail {
           crumbs.length == breadcrumbStates.length,
       'Lists of crumbs, callbacks, and breadcrumb states must be of the same length.',
     );
+    final bool useCrumbSeperator = crumbSeparator != null;
     return Wrap(
       spacing: crumbSpacing,
       children: List.generate(crumbs.length, (index) {
@@ -42,12 +44,14 @@ class BreadcrumbTrail {
         return Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            if (index != 0)
+            if (index != 0 && !useCrumbSeperator)
               Icon(
                 crumbIcon,
                 size: crumbIconSize,
                 color: crumbIconColor,
               ),
+            if (index != 0 && useCrumbSeperator)
+              crumbSeparator,
             SizedBox(width: crumbSpacing),
             GestureDetector(
               onTap: isEnabled ? () async => callbacks[index]() : null,
